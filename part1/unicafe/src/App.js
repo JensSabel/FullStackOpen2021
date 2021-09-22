96487088d27ce5ae16ab86ccaaed709369f03a22
JensSabel <jens.sabel@abo.fi>
2021-09-22 14:05:33 +0300

    Exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
 
 const DisplayHeader = ({text}) => <h1>{text}</h1>
-const DisplayStats = (props) => {
-  return(
-  <div>
-    {props.text}: {props.stats}
-  </div>)
+
+const Statistics = (props) => {
+    const total = props.good + props.neutral + props.bad
+    const average = (props.good - props.bad)/total
+    const positive = props.good/total * 100
+
+    return(
+      <dic>
+        <p>good: {props.good}</p>
+        <p>neutral: {props.neutral}</p>
+        <p>bad: {props.bad}</p>
+        <p>all: {total}</p>
+        <p>average: {average}</p>
+        <p>positive: {positive}</p>
+      </dic>
+    )
 }
+
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
@@ -17,28 +29,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
-  const average = (good-bad)/(good+neutral+bad)
-  const positive = (good/all)*100
-
-  
   const HandleGoodClick = () => setGood(good+1)
   const HandleNeutralClick = () => setNeutral(neutral+1)
   const HandleBadClick = () => setBad(bad+1)
+  
   return (
     <div>
-      <DisplayHeader text = 'Give Feedback'></DisplayHeader>
-      <Button handleClick={HandleGoodClick} text = "Good"></Button>
-      <Button handleClick={HandleNeutralClick} text = "Neutral"></Button>
-      <Button handleClick={HandleBadClick} text = "Bad"></Button>
-      <DisplayHeader text = 'statistics'></DisplayHeader>
-      <DisplayStats text='Good' stats={good}></DisplayStats>
-      <DisplayStats text='Neutral' stats={neutral}></DisplayStats>
-      <DisplayStats text='Bad' stats={bad}></DisplayStats>
-      <DisplayStats text='All' stats={all}></DisplayStats>
-      <DisplayStats text='Average' stats={average}></DisplayStats>
-      <DisplayStats text='Positive' stats={positive}></DisplayStats>
-
+      <DisplayHeader text = 'Give Feedback'/>
+      <Button handleClick={HandleGoodClick} text = "Good"/>
+      <Button handleClick={HandleNeutralClick} text = "Neutral"/>
+      <Button handleClick={HandleBadClick} text = "Bad"/>
+      <DisplayHeader text = 'Statistics'/>
+      <Statistics
+        good = {good}
+        neutral = {neutral}
+        bad = {bad}
+        />
     </div>
   )
 }
